e42311e0c5f93442a2e7748e7209ece091dc9f2c
Mykhailo Zorin <mykhailo.zorin@gmail.com>
2016-11-06 20:00:42 -0600

    prim add

--- maze.js
+++ maze.js
@@ -10,12 +10,14 @@ function Maze (c, r) {
 	this.maze = new Array();
 	
 	this.x_min = 1;
-	this.x_max = this.cols - 2;
 	this.y_min = 1;
+	this.x_max = this.cols - 2;
 	this.y_max = this.rows - 2;
+	//this.x_max = 3;
+	//this.y_max = 3;
 	
-	this.x_start = this.cols - 2;
-	this.y_start = this.rows - 2;
+	this.x_end = 1;
+	this.y_end = 1;
 	
 	/* c-tor */
 	for (i = 0; i < this.rows; ++i) {
@@ -76,6 +78,12 @@ Maze.prototype.clear = function() {
 
 Maze.prototype.rbt = function () {
 	
+	var current_node = { x: this.x_max, y: this.y_max };
+	var potent_nodes = new Array(), is_run = true;
+	var x = 0, y = 0;		
+	
+	this.maze[this.y_start][this.x_start] = this.active;
+
 	function getRandom(min, max) {
 	  
 		min = Math.ceil(min);
@@ -87,15 +95,8 @@ Maze.prototype.rbt = function () {
 	
 		while (potent_nodes.length) { potent_nodes.pop(); }
 	}
-	
-	var current_node = { x: this.x_max, y: this.y_max };
-	// var current_node = { x: 3, y: 3 };
-	// var current_node = { x: 1, y: 1 };
-	var potent_nodes = new Array(), is_run = true;
-	var x = 0, y = 0;		
-	
-	this.maze[this.y_start][this.x_start] = this.active;
 
+	// main cycle
 	do {
 		delAllNodes();
 
@@ -187,12 +188,118 @@ Maze.prototype.rbt = function () {
 	}
 } 
 
+Maze.prototype.prim = function () {
+
+	var current_node = { y: this.y_max, x: this.x_max };
+	var nodes = new Array();
+	var x = 0, y = 0, n = 0, t = 0;		
+	
+	function getRandom(min, max) {
+	  
+		min = Math.ceil(min);
+	  	max = Math.floor(max);
+	  	return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	do {
+		this.maze[current_node.y][current_node.x] = this.back;
+	
+		// check for four directions
+		// N
+		if(current_node.y - 1 > this.y_min 
+		   && this.maze[current_node.y - 2][current_node.x] == this.space) {
+
+			nodes.push( {y: current_node.y - 2, x: current_node.x} );
+			this.maze[current_node.y - 2][current_node.x] = this.active;
+		}
+		// E
+		if(current_node.x + 1 < this.x_max 
+		   && this.maze[current_node.y][current_node.x + 2] == this.space) {
+
+			nodes.push( {y: current_node.y, x: current_node.x + 2} );
+			this.maze[current_node.y][current_node.x + 2] = this.active;
+		}
+		// S
+		if(current_node.y + 1 < this.y_max 
+		   && this.maze[current_node.y + 2][current_node.x] == this.space) {
+
+			nodes.push( {y: current_node.y + 2, x: current_node.x} );
+			this.maze[current_node.y + 2][current_node.x] = this.active;
+		}
+		// W
+		if(current_node.x - 1 > this.x_min 
+		   && this.maze[current_node.y][current_node.x - 2] == this.space) {
+
+			nodes.push( {y: current_node.y, x: current_node.x - 2} );
+			this.maze[current_node.y][current_node.x - 2] = this.active;
+		}
+	
+		
+		if(nodes.length > 0) {
+			
+			n = getRandom(0, nodes.length - 1); 
+			x = nodes[n].x;
+			y = nodes[n].y;
+
+			this.maze[current_node.y][current_node.x] = this.back;
+
+			// digging the wall
+			// N
+			if(y - 1 > this.y_min 
+			   && this.maze[y - 2][x] === this.back
+			   && this.maze[y - 1][x] === this.brick
+			  ) {
+
+				this.maze[y - 1][x] = this.back;
+			}
+			// E
+			else if(x + 1 < this.x_max 
+			   && this.maze[y][x + 1] === this.brick
+			   && this.maze[y][x + 2] === this.back
+			) {
+				
+				this.maze[y][x + 1] = this.back;
+			}
+			
+			// S
+			else if(	y + 1 < this.y_max 
+			   && this.maze[y + 1][x] === this.brick
+			   && this.maze[y + 2][x] === this.back
+			) {
+
+				this.maze[y + 1][x] = this.back;
+			}
+			// W
+			else if(x - 1 > this.x_min 
+			   && this.maze[y][x - 1] === this.brick
+			   && this.maze[y][x - 2] === this.back
+			) {
+
+				this.maze[y][x - 1] = this.back;
+			}
+			current_node = nodes[n];
+			this.maze[current_node.y][current_node.x] = this.back;
+			nodes.splice(n, 1);
+		}
+	} while(nodes.length > 0);
+
+	// replace this.back
+	for (i = 0; i < this.rows; ++i) {
+
+		for (j = 0; j < this.cols; ++j) {
+
+			this.maze[i][j] = this.maze[i][j] == this.back ? this.space : this.maze[i][j];
+		}
+	}
+}
+
 var cols = 97, rows = 35;
-// cols = 57, rows = 31;
+//cols = 57, rows = 31;
 //cols = 7, rows = 7;
 
 var maze = new Maze(cols, rows);
 
-maze.rbt();
+// maze.rbt();
+maze.prim();
 maze.print();
 
