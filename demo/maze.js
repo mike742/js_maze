645f5fd87892c53c56177bc032c39fca7d0c5ac5
Mykhailo Zorin <mykhailo.zorin@gmail.com>
2016-11-20 19:59:46 -0600

    new vertion with div and animated demo

--- demo/maze.js
+++ demo/maze.js
@@ -13,9 +13,8 @@ function Maze (c, r) {
 	this.y_min = 1;
 	this.x_max = this.cols - 2;
 	this.y_max = this.rows - 2;
-	//this.x_max = 3;
-	//this.y_max = 3;
-	
+	this.x_start = this.cols - 2;
+ 	this.y_start = this.rows - 2;	
 	this.x_end = 1;
 	this.y_end = 1;
 	
@@ -54,6 +53,30 @@ Maze.prototype.print = function() {
 	}
 };
 
+Maze.prototype.print_div = function () {
+
+	for (i = 0; i < rows; ++i) {
+		
+		document.write ("<div class='brick' id='first' name='" + i + "_" + j + "'></div>");
+		
+		for (j = 1; j < cols; ++j) {
+
+			switch (this.maze[i][j]) {
+			
+				case 9608: // brick;
+
+					document.write ("<div class='brick' name='" + i + "_" + j + "'></div>");
+					break;
+					
+				case 9617: // space;
+					
+					document.write ("<div class='brick' id='space' name='" + i + "_" + j + "'></div>");
+					break;
+			}
+		}
+	}	
+}
+
 Maze.prototype.clear = function() {
 
 	for (i = 0; i < this.rows; ++i) {
@@ -81,9 +104,22 @@ Maze.prototype.rbt = function () {
 	var current_node = { x: this.x_max, y: this.y_max };
 	var potent_nodes = new Array(), is_run = true;
 	var x = 0, y = 0;		
-	
-	this.maze[this.y_start][this.x_start] = this.active;
+	var t = dt = 100;
+	var name = "";
+	var el = 0; 
 
+	this.maze[this.y_start][this.x_start] = this.active;
+	
+	// change div's id-attr 
+	name = this.y_start + "_" + this.x_start;
+	el = $("div[name = " + name + "]");
+	
+	el.delay(t += dt).queue(function() {
+
+		 $(this).attr('id', "active");
+		 $(this).dequeue();
+	});
+	
 	function getRandom(min, max) {
 	  
 		min = Math.ceil(min);
@@ -129,10 +165,18 @@ Maze.prototype.rbt = function () {
 		if(potent_nodes.length > 0) {
 		
 			// check potential nodes
-			//x =  getRandom(0, potent_nodes.length - 1); 
 			x =  getRandom(0, getRandom(0, potent_nodes.length - 1)); 
 
 			this.maze[ potent_nodes[x].y ][potent_nodes[x].x ] = this.active;
+
+			// change div's id-attr 
+			name = potent_nodes[x].y + "_" + potent_nodes[x].x;
+			el = $("div[name = " + name + "]");
+
+			el.delay(t += dt).queue(function() {
+
+				 $(this).attr('id', "back");
+			});
 
 			// checking next step
 			if(current_node.x == potent_nodes[x].x) {
@@ -146,15 +190,32 @@ Maze.prototype.rbt = function () {
 
 			this.maze[current_node.y][current_node.x] = this.active;
 			
+			name = current_node.y + "_" + current_node.x;
+			el = $("div[name = " + name + "]");
+
+			el.delay(t += dt).queue(function() {
+
+				 $(this).attr('id', "back");
+			});
+			
 		} else {
 			
 			this.maze[current_node.y][current_node.x] = this.back; 
+			
+			name = current_node.y + "_" + current_node.x;
+			el = $("div[name = " + name + "]");
+
+			el.delay(t += dt).queue(function() {
+
+				 $(this).attr('id', "back");
+				 $(this).dequeue();
+			});
+			
 			// return to the previuos node or to the start
 			// N
 			if(this.maze[current_node.y - 1][current_node.x] == this.active) {
 
 				current_node.y -= 1;
-				//this.maze[current_node.y - 1][current_node.x] = this.back; 
 			}
 			// E
 			else if(this.maze[current_node.y][current_node.x + 1] == this.active) {
@@ -183,7 +244,19 @@ Maze.prototype.rbt = function () {
 
 		for (j = 0; j < this.cols; ++j) {
 
+			if(this.maze[i][j] == this.active) {
+			
+				name = i + "_" + j;
+				el = $("div[name = " + name + "]");
+
+				el.delay(t += dt).queue(function() {
+
+					 $(this).attr('id', "space");
+					 $(this).dequeue();
+				});
+			}
 			this.maze[i][j] = this.maze[i][j] == this.back ? this.space : this.maze[i][j];
+			
 		}
 	}
 } 
@@ -194,7 +267,9 @@ Maze.prototype.prim = function () {
 	var p_nodes = new Array();
 	var nodes = new Array();
 	var x = 0, y = 0, n = 0, t = 0;		
-	
+	var time = dt = 100;
+	var name = "";
+	var el = 0; 	
 	function getRandom(min, max) {
 	  
 		min = Math.ceil(min);
@@ -205,6 +280,15 @@ Maze.prototype.prim = function () {
 	do {
 		this.maze[current_node.y][current_node.x] = this.back;
 	
+		name = current_node.y + "_" + current_node.x;
+		el = $("div[name = " + name + "]");
+
+		el.delay(time += dt).queue(function() {
+
+			 $(this).attr('id', "back");
+		});
+		
+		//return;
 		// check for four directions
 		// N
 		if(current_node.y - 1 > this.y_min 
@@ -212,6 +296,15 @@ Maze.prototype.prim = function () {
 
 			nodes.push( {y: current_node.y - 2, x: current_node.x} );
 			this.maze[current_node.y - 2][current_node.x] = this.active;
+			
+			name = (current_node.y - 2) + "_" + current_node.x;
+			el = $("div[name = " + name + "]");
+
+			el.delay(time += dt).queue(function() {
+
+				 $(this).attr('id', "active");
+				 $(this).dequeue();
+			});
 		}
 		// E
 		if(current_node.x + 1 < this.x_max 
@@ -219,6 +312,15 @@ Maze.prototype.prim = function () {
 
 			nodes.push( {y: current_node.y, x: current_node.x + 2} );
 			this.maze[current_node.y][current_node.x + 2] = this.active;
+
+			name = current_node.y + "_" + (current_node.x + 2);
+			el = $("div[name = " + name + "]");
+
+			el.delay(time += dt).queue(function() {
+
+				 $(this).attr('id', "active");
+				 $(this).dequeue();
+			});
 		}
 		// S
 		if(current_node.y + 1 < this.y_max 
@@ -226,6 +328,15 @@ Maze.prototype.prim = function () {
 
 			nodes.push( {y: current_node.y + 2, x: current_node.x} );
 			this.maze[current_node.y + 2][current_node.x] = this.active;
+
+			name = (current_node.y + 2) + "_" + current_node.x;
+			el = $("div[name = " + name + "]");
+
+			el.delay(time += dt).queue(function() {
+
+				 $(this).attr('id', "active");
+				 $(this).dequeue();
+			});
 		}
 		// W
 		if(current_node.x - 1 > this.x_min 
@@ -233,6 +344,15 @@ Maze.prototype.prim = function () {
 
 			nodes.push( {y: current_node.y, x: current_node.x - 2} );
 			this.maze[current_node.y][current_node.x - 2] = this.active;
+
+			name = current_node.y + "_" + (current_node.x - 2);
+			el = $("div[name = " + name + "]");
+
+			el.delay(time += dt).queue(function() {
+
+				 $(this).attr('id', "active");
+				 $(this).dequeue();
+			});
 		}
 	
 		
@@ -243,6 +363,15 @@ Maze.prototype.prim = function () {
 			y = nodes[n].y;
 
 			this.maze[current_node.y][current_node.x] = this.back;
+
+			name = current_node.y + "_" + current_node.x;
+			el = $("div[name = " + name + "]");
+
+			el.delay(time += dt).queue(function() {
+
+				 $(this).attr('id', "back");
+				 $(this).dequeue();
+			});
 
 			// digging the wall
 			
@@ -280,49 +409,79 @@ Maze.prototype.prim = function () {
 				p_nodes.push(4);
 			}
 
-			console.log(p_nodes.length);
-			console.log(getRandom(0, p_nodes.length));
+			var rn = p_nodes[getRandom(0, p_nodes.length - 1)];
+			while (p_nodes.length) { p_nodes.pop(); }
 			
+			switch(rn) {
+				case 1:
+					this.maze[y - 1][x] = this.back;
+
+					name = (y - 1) + "_" + x;
+					el = $("div[name = " + name + "]");
+
+					el.delay(time += dt).queue(function() {
+
+						 $(this).attr('id', "back");
+						 $(this).dequeue();
+					});
+
+					break;
+				case 2:
+					this.maze[y][x + 1] = this.back;
+					
+					name = (y) + "_" + (x + 1);
+					el = $("div[name = " + name + "]");
+
+					el.delay(time += dt).queue(function() {
+
+						 $(this).attr('id', "back");
+						 $(this).dequeue();
+					});
+
+					break;
+
+				case 3:
+					this.maze[y + 1][x] = this.back;
+					name = (y + 1) + "_" + x;
+					el = $("div[name = " + name + "]");
+
+					el.delay(time += dt).queue(function() {
+
+						 $(this).attr('id', "back");
+						 $(this).dequeue();
+					});
+	
+					break;
+				case 4:
+					this.maze[y][x - 1] = this.back;
+					
+					name = (y - 1) + "_" + (x - 1);
+					el = $("div[name = " + name + "]");
+
+					el.delay(time += dt).queue(function() {
+
+						 $(this).attr('id', "back");
+						 $(this).dequeue();
+					});
+
+				break;
+			}
 			
-			// N
-			if(y - 1 > this.y_min 
-			   && this.maze[y - 2][x] === this.back
-			   && this.maze[y - 1][x] === this.brick
-			  ) {
-
-				this.maze[y - 1][x] = this.back;
-			}
-			// E
-			else if(x + 1 < this.x_max 
-			   && this.maze[y][x + 1] === this.brick
-			   && this.maze[y][x + 2] === this.back
-			) {
-				
-				this.maze[y][x + 1] = this.back;
-			}
-			
-			// S
-			else if(	y + 1 < this.y_max 
-			   && this.maze[y + 1][x] === this.brick
-			   && this.maze[y + 2][x] === this.back
-			) {
-
-				this.maze[y + 1][x] = this.back;
-			}
-			// W
-			else if(x - 1 > this.x_min 
-			   && this.maze[y][x - 1] === this.brick
-			   && this.maze[y][x - 2] === this.back
-			) {
-
-				this.maze[y][x - 1] = this.back;
-			}
 			current_node = nodes[n];
 			this.maze[current_node.y][current_node.x] = this.back;
+			
+			name = current_node.y + "_" + current_node.x;
+			el = $("div[name = " + name + "]");
+
+			el.delay(time += dt).queue(function() {
+
+				 $(this).attr('id', "back");
+				 $(this).dequeue();
+			});
+
 			nodes.splice(n, 1);
 		}
-	} while(++t < 2);
-	// } while(nodes.length > 0);
+	} while(nodes.length > 0);
 
 	// replace this.back
 	for (i = 0; i < this.rows; ++i) {
@@ -334,13 +493,12 @@ Maze.prototype.prim = function () {
 	}
 }
 
-var cols = 97, rows = 35;
-//cols = 57, rows = 31;
-cols = 7, rows = 7;
+
+// main
+cols = 9, rows = 7;
 
 var maze = new Maze(cols, rows);
-
-// maze.rbt();
+maze.print_div();
+//maze.rbt();
 maze.prim();
-maze.print();
 
--- maze.js
+++ maze.js
@@ -53,6 +53,30 @@ Maze.prototype.print = function() {
 	}
 };
 
+Maze.prototype.print_div = function () {
+
+	for (i = 0; i < rows; ++i) {
+		
+		document.write ("<div class='brick' id='first' name='" + i + "_" + j + "'></div>");
+		
+		for (j = 1; j < cols; ++j) {
+
+			switch (this.maze[i][j]) {
+			
+				case 9608: // brick;
+
+					document.write ("<div class='brick' name='" + i + "_" + j + "'></div>");
+					break;
+					
+				case 9617: // space;
+					
+					document.write ("<div class='brick' id='space' name='" + i + "_" + j + "'></div>");
+					break;
+			}
+		}
+	}	
+}
+
 Maze.prototype.clear = function() {
 
 	for (i = 0; i < this.rows; ++i) {
@@ -187,6 +211,124 @@ Maze.prototype.rbt = function () {
 	}
 } 
 
+Maze.prototype.rbt_div = function () {
+	
+	var current_node = { x: this.x_max, y: this.y_max };
+	var potent_nodes = new Array(), is_run = true;
+	var x = 0, y = 0;		
+	var t = dt = 100;
+	var name = "";
+	var el = 0; 
+
+	this.maze[this.y_start][this.x_start] = this.active;
+	
+	function getRandom(min, max) {
+	  
+		min = Math.ceil(min);
+	  	max = Math.floor(max);
+	  	return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	
+	function delAllNodes() {
+	
+		while (potent_nodes.length) { potent_nodes.pop(); }
+	}
+
+	// main cycle
+	do {
+		delAllNodes();
+
+		// check for four directions
+		// N
+		if(current_node.y - 1 > this.y_min 
+		   && this.maze[current_node.y - 2][current_node.x] == this.space) {
+
+			potent_nodes.push( {y: current_node.y - 1, x: current_node.x} );
+		}
+		// E
+		if(current_node.x + 1 < this.x_max
+		  && this.maze[current_node.y][current_node.x + 2] == this.space) {
+
+			potent_nodes.push( {y: current_node.y, x: current_node.x + 1} );
+		}
+		// S
+		if(current_node.y + 1 < this.y_max
+		  && this.maze[current_node.y + 2][current_node.x] == this.space) {
+
+			potent_nodes.push( {y: current_node.y + 1, x: current_node.x} );
+		}
+		// W
+		if(current_node.x - 1 > this.x_min
+		  && this.maze[current_node.y][current_node.x - 2] == this.space) {
+
+			potent_nodes.push( {y: current_node.y, x: current_node.x - 1} );
+		}
+
+		if(potent_nodes.length > 0) {
+		
+			// check potential nodes
+			//x =  getRandom(0, potent_nodes.length - 1); 
+			x =  getRandom(0, getRandom(0, potent_nodes.length - 1)); 
+
+			this.maze[ potent_nodes[x].y ][potent_nodes[x].x ] = this.active;
+
+			// checking next step
+			if(current_node.x == potent_nodes[x].x) {
+
+				current_node.y = current_node.y > potent_nodes[x].y ? potent_nodes[x].y - 1 : potent_nodes[x].y + 1; 
+
+			} else if(current_node.y == potent_nodes[x].y) {
+
+				current_node.x = current_node.x > potent_nodes[x].x ? potent_nodes[x].x - 1 : potent_nodes[x].x + 1; 
+			}
+
+			this.maze[current_node.y][current_node.x] = this.active;
+			
+		} else {
+			
+			this.maze[current_node.y][current_node.x] = this.back; 
+			
+			// return to the previuos node or to the start
+			// N
+			if(this.maze[current_node.y - 1][current_node.x] == this.active) {
+
+				current_node.y -= 1;
+				//this.maze[current_node.y - 1][current_node.x] = this.back; 
+			}
+			// E
+			else if(this.maze[current_node.y][current_node.x + 1] == this.active) {
+
+				current_node.x += 1;
+			}
+			// S
+			else if(this.maze[current_node.y + 1][current_node.x] == this.active) {
+
+				current_node.y += 1;
+			}
+			// W
+			else if(this.maze[current_node.y][current_node.x - 1] == this.active) {
+
+				current_node.x -= 1;
+			}
+
+			if (current_node.x == this.x_start && current_node.y == this.y_start) {
+				is_run = false;
+			}
+		}
+	} while ( is_run || ++y < 100);
+	
+	// replace this.back
+	for (i = 0; i < this.rows; ++i) {
+
+		for (j = 0; j < this.cols; ++j) {
+
+			this.maze[i][j] = this.maze[i][j] == this.back ? this.space : this.maze[i][j];
+			
+		}
+	}
+} 
+
+
 Maze.prototype.prim = function () {
 
 	var current_node = { y: this.y_max, x: this.x_max };
@@ -317,16 +459,43 @@ Maze.prototype.prim = function () {
 	}
 }
 
-var cols = 97, rows = 35;
+var cols = 237, rows = 127;
+
 //cols = 57, rows = 31;
-//cols = 7, rows = 7;
+// cols = 9, rows = 7;
 
 var maze = new Maze(cols, rows);
+//maze.rbt();
+maze.prim();
+maze.print_div();
+//maze.print_div();
 
-maze.rbt();
-maze.print();
+/*
+var t = 1000;
+var name = 5 + "_" + 5;
+//el[0].id = "active";
+var el = $("div[name = "+name+"]");
+el.delay(t += t).queue(function() {
 
+	 $(this).attr('id', "active");
+	 $(this).dequeue();
+});
+
+name = 4 + "_" + 5;
+el = $("div[name = "+name+"]");
+el.delay(t += t).queue(function() {
+
+	 $(this).attr('id', "active");
+	 $(this).dequeue();
+});
+*/
+//maze.rbt();
+
+//maze.prim();
+/*
 maze = new Maze(cols, rows);
 maze.prim();
 maze.print();
+*/
+
 
